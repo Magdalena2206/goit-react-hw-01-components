0faa5e4f78b9b64b04adbbb138e3047afccbc86f
Magdalena Uszynska <magdalenauszynska1@gmail.com>
2024-01-14 17:46:58 +0100

    add profile

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,11 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FriendListItem } from 'components/FriendListItem/FriendListItem';
+import { FriendsList } from './FriendList.styled';
+
+export const FriendList = ({friends}) => {
+  return (
+    <FriendsList>
+      {
+        friends.map((friend) => (
+          <FriendListItem
+            key={friend.id}
+            id={friend.id}
+            avatar={friend.avatar}
+            name={friend.name}
+            isOnline={friend.isOnline}/>
+          )
+          )
+      }
+   </FriendsList>
+  )
+}
 
 
-/* 
-<ul>
-<li class="item">
-  <span class="status"></span>
-  <img class="avatar" src="" alt="User avatar" width="48" />
-  <p class="name"></p>
-</li>
-</ul>
- */
+FriendList.propTypes = {
+friends: PropTypes.arrayOf(PropTypes.shape({
+  id: PropTypes.number.isRequired,
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+}).isRequired).isRequired
+
+}
+
+
+
