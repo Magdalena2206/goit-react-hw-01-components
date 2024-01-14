d257c248e6e86b11363fb231ae9cb1eec8b96500
Magdalena Uszynska <magdalenauszynska1@gmail.com>
2024-01-14 19:26:16 +0100

    change app.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -20,52 +20,29 @@ import { ThemeProvider } from "styled-components";
 
 export const App = () => {
   return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-      justifyContent="space-between"
-      as="main"
-      px={5}
-      py={3}
-      width={1} 
-      bg="muted">
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
-       <GlobalStyle />
-    </Box>
-  );
+    <ThemeProvider theme={theme}>
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="space-between"
+        as="main"
+        px={5}
+        py={3}
+        width={1}
+        bg="muted">
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+        <Statistics title="Upload stats" stats={data} />
+        <FriendList friends={friends} />
+        <TransactionHistory items={transactions} />
+        <GlobalStyle />
+      </Box>
+    </ThemeProvider>);
 };
 
-<ThemeProvider theme={theme}>
-<Box
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-      justifyContent="space-between"
-      as="main"
-      px={5}
-      py={3}
-      width={1} 
-      bg="muted">
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
-       <GlobalStyle />
-    </Box>
-</ThemeProvider>
